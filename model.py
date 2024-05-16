5a881e1edc95eea8bb4c39ea1368d90f4656a07f
GabrielSilvaSouza <gabrieldasilvasouza21@gmail.com>
2024-05-16 17:03:52 -0300

    done

---
+++ model.py
@@ -0,0 +1,40 @@
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import torch.optim as optim
+import os
+from PIL import Image
+
+
+
+class FFCNN(nn.Module):
+    def __init__(self):
+        super(FFCNN, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),  # Assuming input images are RGB
+            nn.ReLU(),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 3, kernel_size=3, padding=1),
+            nn.Sigmoid()  # Use sigmoid to ensure output is between 0 and 1
+        )
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+
